4aa0e7f6133a81f17fa63cbdc503d81a2d83dd88
Paweł Gesek <pgesek@soldevelo.com>
2018-03-09 16:40:51 +0100

    ILI-16: Add code analysis

--- api/src/main/java/org/openmrs/module/labintegration/LabIntegrationConfig.java
+++ api/src/main/java/org/openmrs/module/labintegration/LabIntegrationConfig.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -17,5 +17,5 @@ import org.springframework.stereotype.Component;
 @Component("labintegration.LabIntegrationConfig")
 public class LabIntegrationConfig {
 	
-	public final static String MODULE_PRIVILEGE = "Lab Integration Privilege";
+	public static final String MODULE_PRIVILEGE = "Lab Integration Privilege";
 }
