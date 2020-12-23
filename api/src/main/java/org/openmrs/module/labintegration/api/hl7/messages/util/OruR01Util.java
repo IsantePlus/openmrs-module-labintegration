2ce3d42476949af936e00a16de098bab3f19daae
moshonk <omondi.ken@gmail.com>
2020-12-23 16:52:45 +0300

    Modifications to allow processing of HL7 Lab results (ORU_01) from LNSP

--- api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/util/OruR01Util.java
+++ api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/util/OruR01Util.java
@@ -1,14 +1,15 @@
 package org.openmrs.module.labintegration.api.hl7.messages.util;
 
-import ca.uhn.hl7v2.model.v25.segment.OBR;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import ca.uhn.hl7v2.model.v25.segment.OBR;
+import ca.uhn.hl7v2.model.v25.segment.PV1;
 
 public final class OruR01Util {
 
 	private static final Pattern ONLY_UUID = Pattern.
-			compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
+			compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
 	
 	public static final String ORUR01_ORU_R01 = "ORU\\^R01\\^ORU_R01";
 	
@@ -25,12 +26,35 @@ public final class OruR01Util {
 	}
 
 	public static String getUuidFromOBRSegment4(OBR obr) {
-		String segment4 = obr.getObr4_UniversalServiceIdentifier().getIdentifier().getValue();
-		Matcher matcher = ONLY_UUID.matcher(segment4);
+		try {
+			String segment4 = obr.getObr4_UniversalServiceIdentifier().getIdentifier().getValue();
+			Matcher matcher = ONLY_UUID.matcher(segment4);
 
-		if (matcher.find()) {
-			return matcher.group();
-		} else {
+			if (matcher.find()) {
+				return matcher.group();
+			} else {
+				return null;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static String getUuidFromPV1Segment(PV1 pv1) {
+		try {
+			String segment50_5 = pv1.getAlternateVisitID().getCx5_IdentifierTypeCode().getValue();
+			Matcher matcher = ONLY_UUID.matcher(segment50_5);
+
+			if (matcher.find()) {
+				return matcher.group();
+			} else {
+				return null;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
