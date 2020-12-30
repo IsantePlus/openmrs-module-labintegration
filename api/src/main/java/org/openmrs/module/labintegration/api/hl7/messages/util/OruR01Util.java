62d27f375d42c7dab34e007864d1e6d9e29ff803
Ken Omondi <omondi.ken@gmail.com>
2020-12-30 04:16:18 +0300

    Fixed test and checkstyle failures (#17)

--- api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/util/OruR01Util.java
+++ api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/util/OruR01Util.java
@@ -3,21 +3,26 @@ package org.openmrs.module.labintegration.api.hl7.messages.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import ca.uhn.hl7v2.model.v25.segment.OBR;
 import ca.uhn.hl7v2.model.v25.segment.PV1;
 
 public final class OruR01Util {
 
-	private static final Pattern ONLY_UUID = Pattern.
-			compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
-	
 	public static final String ORUR01_ORU_R01 = "ORU\\^R01\\^ORU_R01";
-	
+
 	public static final String ORU_R01 = "ORU^R01";
-	
+
 	public static final String VERSION_25 = "2.5";
-	
+
 	public static final String VERSION_251 = "2.5.1";
+
+	private static final Log LOGGER = LogFactory.getLog(OruR01Util.class);
+
+	private static final Pattern ONLY_UUID = Pattern
+			.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
 
 	public static String changeMessageVersionFrom251To25(String message) {
 		message = message.replaceFirst(ORUR01_ORU_R01, ORU_R01);
@@ -35,13 +40,12 @@ public final class OruR01Util {
 			} else {
 				return null;
 			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			LOGGER.error("An error occured while getting uuid from OBR segment", e);
 			return null;
 		}
 	}
-	
+
 	public static String getUuidFromPV1Segment(PV1 pv1) {
 		try {
 			String segment505 = pv1.getAlternateVisitID().getCx5_IdentifierTypeCode().getValue();
@@ -52,9 +56,8 @@ public final class OruR01Util {
 			} else {
 				return null;
 			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			LOGGER.error("An error occured while getting uuid from PV1 segment", e);
 			return null;
 		}
 	}
