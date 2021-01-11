6476418cd37ba3244a3dc9755eee5032a1b4b298
ckemar <ckemar@gmail.com>
2021-01-11 14:26:48 -0500

    initial commit (#22)

--- api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/gnerators/pid/PidIdHelper.java
+++ api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/gnerators/pid/PidIdHelper.java
@@ -10,6 +10,7 @@ import org.openmrs.module.labintegration.api.hl7.config.HL7Config;
 import org.openmrs.module.labintegration.api.hl7.messages.MessageCreationException;
 import org.openmrs.module.labintegration.api.hl7.messages.util.PatientUtil;
 import org.springframework.stereotype.Component;
+import org.apache.commons.lang3.StringUtils;
 
 @Component
 public class PidIdHelper {
@@ -20,6 +21,7 @@ public class PidIdHelper {
 		String siteCode = "";
 		String uuid = "0e52924e-4ebb-40ba-9b83-b198b532653b";
 		String uuidiSanteId = "0e0c7cc2-3491-4675-b705-746e372ff346";
+		PatientIdentifier isanteIdentifier = null;
 
 		for (LocationAttribute locationAttribute : encounter.getLocation().getAttributes()) {
 
@@ -29,25 +31,24 @@ public class PidIdHelper {
 		}
 
 		PatientIdentifier id = PatientUtil.getPatientIdentifier(patient, hl7Config.getPatientIdentifierTypeUuid());
-		/* pid.getPatientID().getIDNumber().setValue(id.getIdentifier()); */
-		//pid.getPatientID().getIDNumber().setValue(id.getId().toString());
 
-		// old patientID from the legacy system iSante
-		PatientIdentifier patientID = PatientUtil.getPatientIdentifier(patient, uuidiSanteId);
+		for (PatientIdentifier identifier : patient.getIdentifiers()) {
+			if (StringUtils.equals(identifier.getIdentifierType().getUuid(), uuidiSanteId)) {
+				isanteIdentifier = identifier;
+				pid.getPatientID().getIDNumber().setValue(isanteIdentifier.getId().toString());
+				break;
+			}
+		}
 
-		if (patientID != null) {
-			pid.getPatientID().getIDNumber().setValue(patientID.getId().toString());
-		} else {
+		if (isanteIdentifier == null) {
 			pid.getPatientID().getIDNumber().setValue(siteCode + id.getId().toString());
 		}
 
-		//pid.getPatientID().getIDNumber().setValue(siteCode + id.getId().toString());
+
 
 		if (id.getLocation() != null) {
 			pid.getPatientID().getAssigningFacility().getNamespaceID()
 					.setValue(siteCode);
-			/* pid.getPatientID().getAssigningFacility().getNamespaceID()
-			        .setValue(String.valueOf(id.getLocation().getLocationId()));  */
 		}
 	}
 }
