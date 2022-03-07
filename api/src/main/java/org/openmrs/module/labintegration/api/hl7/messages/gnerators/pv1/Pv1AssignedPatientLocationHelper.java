cf61c57f61d3fff5b9db6f4c1eacaf475dca4124
Piotr Mankowski <pmanko@uw.edu>
2022-03-07 10:18:47 -0800

    More cleanup

--- api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/gnerators/pv1/Pv1AssignedPatientLocationHelper.java
+++ api/src/main/java/org/openmrs/module/labintegration/api/hl7/messages/gnerators/pv1/Pv1AssignedPatientLocationHelper.java
@@ -14,14 +14,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Pv1AssignedPatientLocationHelper {
 
-	private static final String ECID_UUID = "05a29f94-c0ed-11e2-94be-8c13b969e334";
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(Pv1AssignedPatientLocationHelper.class);
 
-	public void updateAssignedPatientLocation(PV1 pv1, HL7Config hl7Config, Encounter encounter) throws DataTypeException,
-	        MessageCreationException {
-
-		//Code added to set location id to siteCode
+	public void updateAssignedPatientLocation(PV1 pv1, HL7Config hl7Config, Encounter encounter) throws DataTypeException {
+		// Code added to set location id to siteCode
 		String siteCode = "";
 		String uuid = "0e52924e-4ebb-40ba-9b83-b198b532653b";
 
@@ -37,34 +33,31 @@ public class Pv1AssignedPatientLocationHelper {
 		}
 	}
 
-
-	public void assignedAlternativeVisitId(PV1 pv1, HL7Config hl7Config, Encounter encounter) throws HL7Exception,
-			MessageCreationException {
+	public void assignedAlternativeVisitId(PV1 pv1, HL7Config hl7Config, Encounter encounter) {
 
 		//Code added to set location id to siteCode
 		String siteCode = "";
 		String uuid = "0e52924e-4ebb-40ba-9b83-b198b532653b";
 
-		for (LocationAttribute locationAttribute : encounter.getLocation().getAttributes()) {
-
-			if (locationAttribute.getAttributeType().getUuid().equals(uuid)) {
-				siteCode = locationAttribute.getValueReference();
-			}
-		}
-
 		try {
+			for (LocationAttribute locationAttribute : encounter.getLocation().getAttributes()) {
+				if (locationAttribute.getAttributeType().getUuid().equals(uuid)) {
+					siteCode = locationAttribute.getValueReference();
+				}
+			}
+
 			pv1.getAlternateVisitID().getIDNumber().setValue(encounter.getPatient().getUuid());
 			pv1.getAssignedPatientLocation().getPl1_PointOfCare().setValue(siteCode);
 
-		} catch (Exception ex) {
+			// get encounter type uuid and encounter uuid
+			pv1.getAlternateVisitID().getCheckDigit().setValue(encounter.getEncounterType().getUuid());
+			pv1.getAlternateVisitID().getIdentifierTypeCode().setValue(encounter.getUuid());
+
+			//get location location uuid 'To be tested'
+			pv1.getAlternateVisitID().getAssigningAuthority().parse(encounter.getLocation().getUuid());
+		}
+		catch (Exception ex) {
 			LOGGER.error("Could not create PV1 message! \n" + ex.getLocalizedMessage());
 		}
-
-		// get encounter type uuid and encounter uuid
-		pv1.getAlternateVisitID().getCheckDigit().setValue(encounter.getEncounterType().getUuid());
-		pv1.getAlternateVisitID().getIdentifierTypeCode().setValue(encounter.getUuid());
-
-		//get location location uuid 'To be tested'
-		pv1.getAlternateVisitID().getAssigningAuthority().parse(encounter.getLocation().getUuid());
 	}
 }
