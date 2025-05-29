77db90050ed50b60778d25fcc5e6e7a12a75bc2b
mozzy11 <mozzymutesa@gmail.com>
2025-05-29 11:03:02 +0300

    cleanup unused comment

--- api/src/main/java/org/openmrs/module/labintegration/api/advice/EncounterServiceRoundAdvice.java
+++ api/src/main/java/org/openmrs/module/labintegration/api/advice/EncounterServiceRoundAdvice.java
@@ -28,7 +28,6 @@ import org.apache.commons.logging.LogFactory;
 
 public class EncounterServiceRoundAdvice extends StaticMethodMatcherPointcutAdvisor implements Advisor {
 	
-	// we no longer register this aop round advice since it interfers with the proper functioning of the saveEncounter() in OpenMRS core ie in the ED module
 	private OrderService orderService;
 	
 	private AdministrationService administrationService;
